5de9a5a0d540c1de98b7f723dbf9e9990aa22c41
info182 <info@cansaralesdeia.com>
2025-08-12 15:09:07 +0200

    app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,3 @@
 import type { AppProps } from 'next/app'
-import '@/src/styles/globals.css'
+import '../globals.css'
 export default function App({ Component, pageProps }: AppProps) { return <Component {...pageProps} /> }
